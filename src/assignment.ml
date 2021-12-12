1c9c8f3c305d240c1be81003b668721432548103
antonioimola <imola.antonio@gmail.com>
2021-12-13 00:15:56 +0100

    Hallo

--- src/assignment.ml
+++ src/assignment.ml
@@ -1,1 +1,4 @@
-let interleave3 l1 l2 l3 = failwith "TODO interleave3"
+let rec il3 l1 l2 l3 count acc = match l1 with [] -> if count = 2 then acc else il3 l2 l3 l1 (count+1) acc
+| x::xs -> il3 l2 l3 xs 0 (acc @ [x])
+
+let interleave3 l1 l2 l3 = il3 l1 l2 l3 0 []

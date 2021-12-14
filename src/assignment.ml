c0e256f63a95ea1c4367389cf682372753bbc908
antonioimola <imola.antonio@gmail.com>
2021-12-14 13:49:02 +0100

    Hallo

--- src/assignment.ml
+++ src/assignment.ml
@@ -1,4 +1,4 @@
-let rec il3 l1 l2 l3 count acc = match l1 with [] -> if count = 2 then acc else il3 l2 l3 l1 (count+1) acc
-| x::xs -> il3 l2 l3 xs 0 (acc @ [x])
+let rec il3 l1 l2 l3 count acc = match l1 with [] -> if count = 2 then List.rev acc else il3 l2 l3 l1 (count+1) acc
+| x::xs -> (il3[@tailcall]) l2 l3 xs 0 (x::acc)
 
 let interleave3 l1 l2 l3 = il3 l1 l2 l3 0 []
